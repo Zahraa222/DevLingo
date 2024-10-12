84b3f39f0944fa2042fddea61b658ec8eb346878
Celine Chahine <cchahine@umich.edu>
2024-10-12 19:55:34 -0400

    login button works

--- devlingo/app/page.js
+++ devlingo/app/page.js
@@ -1,22 +1,34 @@
 'use client';
 
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './sign-up/login';
 import Register from './sign-up/register';
+import Home from './home';
+
 
 const App = () => {
- return (
-   <Router>
-     <Routes>
-       <Route path="/" element={<Login />} />
-       <Route path="/register" element={<Register />} />
-     </Routes>
-   </Router>
- );
+return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  </Router>
+);
 };
 
 
+
+
 export default App;
+
+
+
+
+
+
 
 
 
