2ae7c376de3ff7b000d031276e5431d9b1c13d5e
Heba <hebasayed1603@gmail.com>
2024-10-12 13:39:11 -0400

    language options

---
+++ devlingo/languages.js
@@ -0,0 +1,26 @@
+// languages.js
+import pythonImage from "./app/images/python.png";
+import javascriptImage from "./app/images/java-script.png";
+import cppImage from "./app/images/c-.png";
+import javaImage from "./app/images/java.png";
+
+const languages = [
+  {
+    imageUrl: pythonImage, // Use imported images
+    text: "Python",
+  },
+  {
+    imageUrl: javascriptImage,
+    text: "JavaScript",
+  },
+  {
+    imageUrl: cppImage,
+    text: "C++",
+  },
+  {
+    imageUrl: javaImage,
+    text: "Java",
+  },
+];
+
+export default languages;
