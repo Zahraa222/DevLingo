a2719c68fb401ed1c6af099de98db429490760d9
Heba <hebasayed1603@gmail.com>
2024-10-12 13:38:37 -0400

    header file

---
+++ devlingo/app/components/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "10px",
+        left: "10px",
+        fontSize: "24px",
+        fontWeight: "bold",
+      }}
+    >
+      Dev Lingo
+    </div>
+  );
+}
+
+export default Header;
