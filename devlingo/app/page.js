3f4880522c62358088f67fb372f44eb2718d22ef
Celine Chahine <cchahine@umich.edu>
2024-10-12 20:15:54 -0400

    added AI button

--- devlingo/app/page.js
+++ devlingo/app/page.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './sign-up/login';
 import Register from './sign-up/register';
 import Home from './home';
+import GetHelp from './sign-up/gethelp';
 
 
 const App = () => {
@@ -14,6 +15,7 @@ return (
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/gethelp" element={<GetHelp />} />
     </Routes>
   </Router>
 );
